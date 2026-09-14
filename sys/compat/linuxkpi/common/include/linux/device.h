d02636c67ce42eb89fc62c020ad0ba8fbe6ae962
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2026-09-14 21:50:20 +0200

    Reapply "linuxkpi: Add device under parent, not under class"
    This reverts commit b0094b5b7320fc40ca581f9ac7c52514ddb5c2cb.

--- sys/compat/linuxkpi/common/include/linux/device.h
+++ sys/compat/linuxkpi/common/include/linux/device.h
@@ -458,12 +458,10 @@ device_add(struct device *dev)
 		if (dev->devt == 0)
 			dev->devt = makedev(0, device_get_unit(dev->bsddev));
 	}
-
-	/*
-	 * Do not change:
-	 * the specific location is used by IB user space tools (PR298485).
-	 */
-	kobject_add(&dev->kobj, &dev->class->kobj, dev_name(dev));
+	if (dev->parent != NULL)
+		kobject_add(&dev->kobj, &dev->parent->kobj, dev_name(dev));
+	else
+		kobject_add(&dev->kobj, &dev->class->kobj, dev_name(dev));
 
 	if (dev->groups)
 		return (sysfs_create_groups(&dev->kobj, dev->groups));
